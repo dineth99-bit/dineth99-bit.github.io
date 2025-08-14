d9dbb2ecd01ff07f47627704e5c90e26f9cc3637
Dineth Jayakody <102501477+dineth99-bit@users.noreply.github.com>
2025-08-14 00:30:38 -0400

    name

--- components/footer.js
+++ components/footer.js
@@ -10,7 +10,7 @@ class Footer extends HTMLElement {
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Your Name</p>
+                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Dineth Jayakody</p>
                     <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
 
                 </div>
