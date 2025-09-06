80c42857221faa027c2adfc79c75d62115ec17fd
Dineth Jayakody <102501477+dineth99-bit@users.noreply.github.com>
2025-09-05 21:25:23 -0400

    new changes

--- components/footer.js
+++ components/footer.js
@@ -11,19 +11,17 @@ class Footer extends HTMLElement {
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
                     <p class="mb-0" style="color: var(--light-gray);">© 2024 Dineth Jayakody</p>
-                    <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
+                    <p class="my-0 pt-0"><a class="" href="mailto:djaya003@odu.edu">djaya003@odu.edu</a></p>
 
                 </div>
-
-
                 <div class="col col-md-7 text-right">
                     <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
                     <p class="my-0 pt-0">
-                        <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
+                        <a href="/"><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/dineth-jayakody/" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://scholar.google.com/citations?user=605obQkAAAAJ&hl=en" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
+                        <a href="https://github.com/dineth99-bit" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
+                        <a href="https://x.com/Dinethjkd" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
                     </p>
                 </div>
             </div>
